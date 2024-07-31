c1aac9a38472ea1101fe2088eb31f0b5452ff289
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-07-31 12:16:50 +0100

    Added remaining methods to player class

--- models/Player.js
+++ models/Player.js
@@ -25,44 +25,70 @@ export default class Player{
     }
 
     listItems(){
-        console.log(`Your Items | ${this.items.length} / 3`)
-        this.items.map(item => console.log(item))//TODO: call item method to display each
+        console.log(`Your bag contains ${this.items.length} items.`)
+        this.items.map(item => console.log(item.getDetails()))
     }
 
     addItem(itemObj){
-        if(this.items.length < 3){
-            this.items.push(itemObj)
-            return
-        }
-
-        let decisionMade = false
-        const rl = readline.createInterface({
-            //TODO: install and import readline
-            input: process.stdin,
-            output: process.stdout,
-        })
-        while(!decisionMade){
-            const message = "Your items bag is full. Would you like to " 
-                + "leave the item (leave) or use the item (use)?"
-            rl.question(message, input => {
-                switch(input){
-                    case "leave":
-                        decisionMade = true
-                        break
-                    case "use":
-                        // useItem(itenObj)
-                        decisionMade = true
-                        break
-                    default:
-                        console.log("Command not recognised.")
-                        break
-                }
-                rl.close()
-            })
-        }
+        this.items.push(itemObj)
     }
 
     useItem(itemObj){
+        if(itemObj.type !== "consumable"){
+            console.log("Select a consumable item to use.")
+            return
+        }
+
+        switch(itemObj.title){
+            case "Apple":
+                this.hp += 1
+                break
+            case "Slice of Pie":
+                this.hp += 2
+                break
+            //TODO: add effects for other items
+        }
+    }
+
+    checkItem(itemObj){
+        return this.items.includes(itemObj)
+    }
+
+    listSpells(){
+        console.log(`Your know ${this.spells.length} spells`)
+        this.spells.map(spell => console.log(spell.getDetails()))
+    }
+
+    checkSpell(spellObj){
+        return this.spells.includes(spellObj)
+    }
+
+    switchWeapon(weaponObj){
+        console.log("Current weapon: " + this.weapon.getDetails())
+        console.log("New weapon: " + weaponObj.getDetails())
+
+        //TODO: install and import readline
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+        let decisionMade = false
+        let message = "Are you sure you want to switch to the new weapon? (Y/N)"
         
+        while(!decisionMade){
+            rl.question(message, input => {
+                if(input.toLowerCase() === "y" || input.toLowerCase() === "yes"){
+                    this.weapon = weaponObj
+                    decisionMade = true
+                    console.log("Equipping new weapon.")
+                }
+                else if(input.toLowerCase() === "n" || input.toLowerCase() === "no"){
+                    decisionMade = true
+                    console.log("New weapon discarded.")
+                }
+                else{message = "Response not recognised, please input 'yes' or 'no'"}
+            })
+        }
+        rl.close()
     }
 }
